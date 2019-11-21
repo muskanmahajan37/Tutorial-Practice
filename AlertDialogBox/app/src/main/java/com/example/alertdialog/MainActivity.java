7d55974f8fae50af14221ba75b08450b1449ad65
Batchu Thrishna <batchuthrishna@gmail.com>
2019-11-21 21:13:10 +0530

    Basic Alert Dialog Box

---
+++ AlertDialogBox/app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                alertBuilder.setTitle("Alert Box")
+                            .setMessage("This is the Alert Dialog Box")
+                            .setCancelable(false)
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(MainActivity.this, "This is Toast", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(MainActivity.this, "This is Toast for NO", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                AlertDialog dialog = alertBuilder.create();
+                dialog.show();
+
+            }
+        });
+
+    }
+}
